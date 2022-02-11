aaf0168123a10b2f679081de870ebf1bbe34d0e6
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-02-11 01:14:01 -0300

    fix: correct base url for api service

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,10 +3,9 @@ import Header from '../../components/Header';
 import Product from './Product';
 import { Container } from './style';
 
-import CartContext from '../../contexts/CartContext';
+import api from '../../services/api';
 
 export default function Cart() {
-
   useEffect(getCart, []);
   //contexto para itens no carrinho
 
@@ -14,29 +13,24 @@ export default function Cart() {
     //GET cart
     //if logged in, get through api
 
-    const cartIds = JSON.parse(localStorage.getItem('cart'));
-    const cart = [];
-    
-    console.log(cartIds)
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    const cartIds = [];
 
-    console.log(cartIds.length)
+    console.log(cart);
 
-    console.log(cart)
+    cart.map((item) => {
+      cartIds.push(item.id);
+    });
+    console.log(cartIds);
 
-    /*cart.map(item => {
-            if(id === item.id) {
-                const source = {quantity: item.quantity + 1}
-                item = Object.assign( item, source)
-                return;
-            }
-            item.push({id: id, quantity: 1})
-        }) */
+    getProductData();
 
     //else, get from localstorage
     //either way, we end up with product ids for cart
   }
 
-  function getProductData() {
+  function getProductData(ids) {
+    const promise = api.getProductsById(ids);
     //send ids to api
     //map res array into Product component
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/products'
+const BASE_URL = 'http://localhost:5000'
 
 function addToCart(id) {
     console.log(id)
