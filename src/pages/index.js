bee8c01a59477f36d1ada66076adcc7467f718d2
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-02-12 16:43:36 -0300

    Bugs Fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Products from "./Products";
 import Product from "./Product";
+import Cart from "./Cart";
 
-export { Home, SignUp, SignIn, Products, Product };
+export { Home, SignUp, SignIn, Products, Product, Cart };
