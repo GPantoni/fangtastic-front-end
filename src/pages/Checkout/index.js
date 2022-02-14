dc774ec589a05b5e1ccc982a1731b378bc33f0a9
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-02-14 11:58:21 -0300

    feat: send and receive forms of payment

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 //add sweetalert lib for confirm
 
 import AuthContext from '../../contexts/AuthContext';
@@ -8,30 +9,42 @@ import Header from '../../components/Header';
 
 import { Container } from './style';
 import FormOfPayment from './FormOfPayment';
+import api from '../../services/api';
 
 export default function Checkout() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { token } = useContext(AuthContext);
   const [hide, setHide] = useState(true);
 
   const [formsOfPayment, setFormsOfPayment] = useState(null);
 
-
   useEffect(getFormsOfPayment, []);
 
-  function getFormsOfPayment() {}
+  function getFormsOfPayment() {
+    if (token) {
+      const promise = api.getFormsOfPaymentData(token);
+      promise.then((res) => setFormsOfPayment(res.data));
+    }
+  }
 
   function handleClickAdd(e) {
     e.preventDefault();
-    navigate(`/add-form-of-payment/${e.target.value}`)
+    navigate(`/add-form-of-payment/${e.target.value}`);
   }
   function handleClickOrder() {}
-  //verificar login
-  //se nao há login, falar pra fazer
-  //verificar formas de pagamento registradas pelo usuario
-  //caso nao exista nenhuma, só aparece o botão de adicionar forma de pagamento
-  //caso sim, usar um radio para selecionar uma das formas de pagamento e confirmar o pedido
-  //enviar objeto com timestamp para poder organizar o histórico de compras
+
+  if (!token) {
+    return (
+      <p>
+        Por favor, <Link to='sign-in'>faça login</Link> ou{' '}
+        <Link to='sign-up'>cadastre-se</Link>.
+      </p>
+    );
+  }
+  if (!formsOfPayment) {
+    return '';
+  }
+
   return (
     <>
       <Header />
@@ -43,14 +56,18 @@ export default function Checkout() {
             ''
           ) : (
             <>
-              <button value='credit' onClick={(e) => handleClickAdd(e)}>Cartão de crédito</button>
-              <button value='blood' onClick={(e) => handleClickAdd(e)}>Pacto de sangue</button>
+              <button value='credit' onClick={(e) => handleClickAdd(e)}>
+                Cartão de crédito
+              </button>
+              <button value='blood' onClick={(e) => handleClickAdd(e)}>
+                Pacto de sangue
+              </button>
             </>
           )}
         </form>
-
-        <FormOfPayment />
-        <FormOfPayment form='slip' />
+        {formsOfPayment.map((data) => (
+          <FormOfPayment {...data} />
+        ))}
       </Container>
     </>
   );
